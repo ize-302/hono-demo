fc87d6409ecd7d5854ad0275ad7e8729f6fcfea6
ize-302 <adavize302@gmail.com>
2024-09-14 14:30:33 +0100

    build: replace bun with npm 🛠

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
+import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hono meets Node.js!')
 })
+ 
+const port = process.env.PORT || 3000
+console.log(`🔥 Server is running on port ${port}`)
 
-export default app
+serve({
+  fetch: app.fetch,
+  port
+})
